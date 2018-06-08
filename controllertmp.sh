7500728ad9ff80c22ba3106a602bf28e192cce01
sunkyu kim <sunkyu4276@gmail.com>
2018-06-08 17:39:23 +0900

    controller tmp

--- controllertmp.sh
+++ controllertmp.sh
@@ -1,5 +1,3 @@
 
-CUDA_VISIBLE_DEVICES=1 python spinn_chemprot.py --genefusion_expstr=DL_train4 --logdir=logs/DL_train4_9 --epochs=25
-CUDA_VISIBLE_DEVICES= python spinn_chemprot.py --genefusion_expstr=DL_train4 --logdir=logs/DL_train4_9 --test_bool
-CUDA_VISIBLE_DEVICES=1 python spinn_chemprot.py --genefusion_expstr=DL_train4 --logdir=logs/DL_train4_10 --epochs=25
-CUDA_VISIBLE_DEVICES= python spinn_chemprot.py --genefusion_expstr=DL_train4 --logdir=logs/DL_train4_10 --test_bool
+CUDA_VISIBLE_DEVICES=0 python spinn_chemprot.py --genefusion_expstr=DL_train4 --logdir=logs/DL_train4_10tmp --epochs=25
+CUDA_VISIBLE_DEVICES= python spinn_chemprot.py --genefusion_expstr=DL_train4 --logdir=logs/DL_train4_10tmp --test_bool
